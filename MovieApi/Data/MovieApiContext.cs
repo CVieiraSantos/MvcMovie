eb1f88e77863ea81f176ad52c1f4c1b93cb83e8d
CVieiraSantos <cadu.vieira2276@gmail.com>
2021-08-24 13:23:59 -0300

    Criacao da controller MoviesController referenciando o projeto MvcMovie

---
+++ MovieApi/Data/MovieApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MovieApi.Data
+{
+    public class MovieApiContext : DbContext
+    {
+        public MovieApiContext (DbContextOptions<MovieApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
